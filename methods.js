d1f0dddfc9fadb51838a18b7304cee8fa742c377
Anderson Keller <63135895+AndersonKeller@users.noreply.github.com>
2023-09-26 21:03:52 -0300

    Add files via upload

---
+++ methods.js
@@ -0,0 +1,24 @@
+let array = ["a","b","c","d","e"]
+
+function findindex(list=[],element){
+    let res = -1
+    for(let i=0;i<list.length;i++){
+        if(list[i]==element){
+            res = i
+        }
+    }
+    return res
+}
+function slice(list=[],index){
+    let aux = []
+    for(let i=0;i<list.length;i++){
+        if(i!=index){
+            aux.push(list[i])
+        }
+    }
+    return aux
+}
+// const res = array.findIndex((item)=>item=="b")
+// console.log(res)
+const res = array.slice(1)
+console.log(res)
--- script.js
+++ script.js
@@ -1,60 +1,79 @@
+// import { data } from "./data.js";
+// const main = document.querySelector("main");
+// const ulData = document.createElement("ul");
+// const btnAdd = document.createElement("button");
+// btnAdd.innerText = "Adicionar";
+// //CRIAR UMA LET A PARTIR DA DATA, PRA PODER MEXER NESSE ARRAY
+// let newData = [...data];
+
+// renderData(newData);
+
+// //CRIEI UM PARAMETRO PRA FUNÇAO DE RENDER, PRA NÃO FICAR SEMPRE
+// // RENDERIZANDO O MESMO ARRAY
+// function renderData(list = []) {
+//   ulData.innerHTML = "";
+//   list.forEach((item) => {
+//     const liData = document.createElement("li");
+//     const priceText = document.createElement("p");
+//     const btnRemove = document.createElement("button");
+
+//     btnRemove.innerText = "X";
+//     priceText.innerText = item.price;
+//     liData.innerText = item.name;
+
+//     liData.id = item.name;
+//     btnRemove.id = item.name;
+
+//     btnRemove.addEventListener("click", () => {
+//       removeItem(btnRemove.id);
+//     });
+
+//     liData.append(priceText, btnRemove);
+//     ulData.appendChild(liData);
+//     ulData.appendChild(btnAdd);
+//   });
+// }
+
+// //https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/splice
+// function removeItem(id) {
+//   //FINDINDEX ENCONTRA O INDEX DE UM DETERMINADO ITEM DENTRO DO ARRAY
+//   const index = newData.findIndex((item) => {
+//     return item.name == id;
+//   });
+//   console.log(index);
+//   //SPLICE RECORTA O NOSSO ARRAY, A PARTIR DO INDEX QUE EU PASSO POR
+//   //PARAMETRO
+//   newData.splice(index, 1);
+//   console.log(newData);
+//   renderData(newData);
+// }
+// ulData.appendChild(btnAdd);
+// main.appendChild(ulData);
+
+// btnAdd.addEventListener("click", () => {
+//   const obj = {
+//     name: "Sofá",
+//     price: "1800",
+//   };
+//   newData.push(obj);
+//   renderData(newData);
+// });
+
+// uma lista de 4 produtos
+// cada produto vai ter um botão de adicionar ao carrinho
+// quando clicar no adicionar
+// outra lista representando o carrinho
 import { data } from "./data.js";
 const main = document.querySelector("main");
-const ulData = document.createElement("ul");
-const btnAdd = document.createElement("button");
-btnAdd.innerText = "Adicionar";
-//CRIAR UMA LET A PARTIR DA DATA, PRA PODER MEXER NESSE ARRAY
-let newData = [...data];
-
-renderData(newData);
-
-//CRIEI UM PARAMETRO PRA FUNÇAO DE RENDER, PRA NÃO FICAR SEMPRE
-// RENDERIZANDO O MESMO ARRAY
-function renderData(list = []) {
-  ulData.innerHTML = "";
-  list.forEach((item) => {
-    const liData = document.createElement("li");
-    const priceText = document.createElement("p");
-    const btnRemove = document.createElement("button");
-
-    btnRemove.innerText = "X";
-    priceText.innerText = item.price;
-    liData.innerText = item.name;
-
-    liData.id = item.name;
-    btnRemove.id = item.name;
-
-    btnRemove.addEventListener("click", () => {
-      removeItem(btnRemove.id);
-    });
-
-    liData.append(priceText, btnRemove);
-    ulData.appendChild(liData);
-    ulData.appendChild(btnAdd);
-  });
+renderData()
+function renderData(){
+    const ul = document.querySelector(".products")
+    data.forEach((item)=>{
+        const li = document.createElement("li")
+        li.innerText = item.name
+        const btnAdd = document.createElement("button")
+        btnAdd.innerText = "Adicionar"
+        li.appendChild(btnAdd)
+        ul.appendChild(li)
+    })
 }
-
-//https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/splice
-function removeItem(id) {
-  //FINDINDEX ENCONTRA O INDEX DE UM DETERMINADO ITEM DENTRO DO ARRAY
-  const index = newData.findIndex((item) => {
-    return item.name == id;
-  });
-  console.log(index);
-  //SPLICE RECORTA O NOSSO ARRAY, A PARTIR DO INDEX QUE EU PASSO POR
-  //PARAMETRO
-  newData.splice(index, 1);
-  console.log(newData);
-  renderData(newData);
-}
-ulData.appendChild(btnAdd);
-main.appendChild(ulData);
-
-btnAdd.addEventListener("click", () => {
-  const obj = {
-    name: "Sofá",
-    price: "1800",
-  };
-  newData.push(obj);
-  renderData(newData);
-});
