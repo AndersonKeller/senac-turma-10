b91b866758a24a43c29f7eba62d8c93fc3b2a09b
AndersonKeller <andersonkellervendas@hotmail.com>
2023-10-10 21:49:36 -0300

    aula-1010

--- desafio/data.js
+++ desafio/data.js
@@ -6,10 +6,10 @@ export const data = {
                 "id": 2321312,
                 "name": "Smartphone Apple iPhone 7 128GB",
                 "images": [
-                    "http://thumbs.buscape.com.br/celular-e-smartphone/smartphone-apple-iphone-7-128gb_600x600-PU98460_1.jpg",
-                    "http://thumbs.buscape.com.br/celular-e-smartphone/smartphone-apple-iphone-7-128gb/__200x400-PU98460_2_c.jpg?v=2347575274",
-                    "http://thumbs.buscape.com.br/celular-e-smartphone/smartphone-apple-iphone-7-128gb/__200x400-PU98460_3_c.jpg?v=318433138",
-                    "http://thumbs.buscape.com.br/celular-e-smartphone/smartphone-apple-iphone-7-128gb/__200x400-PU98460_4_c.jpg?v=33273730"
+                    "https://cdn.awsli.com.br/800x800/2083/2083422/produto/121870080/3f2ccf2606.jpg",
+                    "https://cdn.awsli.com.br/800x800/2083/2083422/produto/121870080/3f2ccf2606.jpg",
+                    "https://cdn.awsli.com.br/800x800/2083/2083422/produto/121870080/3f2ccf2606.jpg",
+                    "https://cdn.awsli.com.br/800x800/2083/2083422/produto/121870080/3f2ccf2606.jpg"
                 ],
                 "price": {
                     "value": 3509.10,
@@ -23,10 +23,10 @@ export const data = {
                 "id": 9971,
                 "name": "Smart TV Samsung Série 4 UN32J4300AG 32 polegadas LED Plana",
                 "images": [
-                    "http://mthumbs.buscape.com.br/tv/smart-tv-samsung-serie-4-un32j4300ag-32-polegadas-led-plana_600x600-PU95547_1.jpg",
-                    "http://thumbs.buscape.com.br/__400x400-PU95547_2_c.jpg?v=3988579075",
-                    "http://thumbs.buscape.com.br/__400x400-PU95547_4_c.jpg?v=4208003525",
-                    "http://thumbs.buscape.com.br/__231312400x400-PU95547_5_c.jpg?v=1473261122"
+                    "https://images-americanas.b2w.io/produtos/4955585321/imagens/smart-tv-65-samsung-led-crystal-4k-uhd-tizen-bluetooth-wi-fi-borda-ultrafina-beahvggxzd/4955585321_1_large.jpg",
+                    "https://images-americanas.b2w.io/produtos/4955585321/imagens/smart-tv-65-samsung-led-crystal-4k-uhd-tizen-bluetooth-wi-fi-borda-ultrafina-beahvggxzd/4955585321_1_large.jpg",
+                    "https://images-americanas.b2w.io/produtos/4955585321/imagens/smart-tv-65-samsung-led-crystal-4k-uhd-tizen-bluetooth-wi-fi-borda-ultrafina-beahvggxzd/4955585321_1_large.jpg",
+                    "https://images-americanas.b2w.io/produtos/4955585321/imagens/smart-tv-65-samsung-led-crystal-4k-uhd-tizen-bluetooth-wi-fi-borda-ultrafina-beahvggxzd/4955585321_1_large.jpg"
                 ],
                 "price": {
                     "value": 1139.90,
@@ -40,7 +40,7 @@ export const data = {
                 "id": 6717131,
                 "name": "Câmera Digital Canon EOS Rebel T5i 18.0 Megapixels",
                 "images": [
-                    "http://mthumbs.buscape.com.br/camera-digital/canon-eos-rebel-t5i-18-0-megapixels_600x600-PU7bf7b_1.jpg"
+                    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSy6YHe7zcV4PXW5lcciUYv7IYUIF0DOL_eqg&usqp=CAU"
                 ],
                 "price": {
                     "value": 1999.20,
@@ -54,7 +54,7 @@ export const data = {
                 "id": 6717131,
                 "name": "Lenovo IdeaPad 310 80UH0004BR Intel Core i7-6500U 2.5 GHz 8192 MB 1024 GB",
                 "images": [
-                    "http://mthumbs.buscape.com.br/notebook/lenovo-ideapad-310-80uh0004br-intel-core-i7-6500u-2-5-ghz-8192-mb-1024-gb_600x600-PU98e6e_1.jpg"
+                    "https://m.media-amazon.com/images/I/51ZBK1ZXkDL._AC_UF894,1000_QL80_.jpg"
                 ],
                 "price": {
                     "value": 2599.00,
--- desafio/script.js
+++ desafio/script.js
@@ -1,2 +1,42 @@
 import {data} from "./data.js"
-console.log(data)
+// console.log(data)
+function main(){
+    const ul = document.querySelector(".ul-products")
+    data.items.forEach((item)=>{
+        ul.insertAdjacentHTML("beforeend",`
+        <li class="li-items">
+            <div class="images">
+                <ul class="gallery">
+                
+                </ul>
+                <img src=${item.product.images[0]} alt="">
+            </div>
+            <div class="description">
+                <h3>${item.product.name}</h3>
+                <div class="div-text-btn">
+                    <div class="texts-prod">
+                    <div><span class="best-price">melhor preço</span></div>
+                    <p><span>${item.product.price.installments}X R$</span>${item.product.price.installmentValue}</p>
+                    <p><span>ou</span>R$ ${item.product.price.value}<span>à vista</span></p>
+                    </div>
+                    <button class="btn-add">Adicionar ao carrinho</button>
+                </div>
+            </div>
+        </li>
+        `)
+        renderGallery(item.product.images)
+    })
+}   
+main()
+function renderGallery(item){
+    console.log(item)
+    const ul = document.querySelector(".gallery")
+    item.forEach((img)=>{
+        console.log(img)
+        // 
+        ul.insertAdjacentHTML("beforeend",`
+        <li>
+            <img src="${img}">
+        </li>
+        `)
+    })}
